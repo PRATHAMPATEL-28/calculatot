f2ff86d33c28746403c0c4bf34c9e11d194ade07
PrathamPatel <84891149+PRATHAMPATEL-28@users.noreply.github.com>
2022-03-01 13:03:41 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+print ("calculator")
+print("Plus Enter 1 & Minus Enter 2")
+
+type1 = (input())
+
+if type1 == "1":
+    print("plus")
+    number1 = int(input('n1'))
+    number2 = int(input('n2'))
+    print (number1 + number2) 
+    exit()
+else:
+    print('not type')
